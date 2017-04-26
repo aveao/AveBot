ff58de1d7eae25d678ebc87c72e914edf498b688
Avery O <ave@ave.zone>
2017-04-26 15:12:51 +0300

    Improve links to linguistics stuff

--- avebot.py
+++ avebot.py
@@ -214,7 +214,7 @@ async def on_message(message):
             toquery = message.content.replace(">similar ", "")
             output = urllib.request.urlopen("https://api.datamuse.com/words?ml="+toquery.replace(" ","+")).read()
             j = json.loads(output)
-            em = discord.Embed(title="Similar word: " + j[0]["word"], description="(more on <http://www.onelook.com/?w="+toquery.replace(" ","+")+"&ls=a>)", colour=0xDEADBF)
+            em = discord.Embed(title="Similar word: " + j[0]["word"], description="(more on <http://www.onelook.com/thesaurus/?s="+toquery.replace(" ","_")+"&loc=cbsim>)", colour=0xDEADBF)
             em.set_author(name='AveBot', icon_url='https://s.ave.zone/c7d.png')
             await client.send_message(message.channel, embed=em)
         elif message.content.startswith('>typo'):
@@ -241,7 +241,7 @@ async def on_message(message):
             toquery = message.content.replace(">rhyme ", "")
             output = urllib.request.urlopen("https://api.datamuse.com/words?rel_rhy="+toquery.replace(" ","+")).read()
             j = json.loads(output)
-            em = discord.Embed(title="Rhymes with: " + j[0]["word"], description="(more on <http://www.onelook.com/?w="+toquery.replace(" ","+")+"&ls=a>)", colour=0xDEADBF)
+            em = discord.Embed(title="Rhymes with: " + j[0]["word"], description="(more on <http://www.rhymezone.com/r/rhyme.cgi?Word="+toquery.replace(" ","+")+"&typeofrhyme=adv&org1=syl&org2=l&org3=y>)", colour=0xDEADBF)
             em.set_author(name='AveBot', icon_url='https://s.ave.zone/c7d.png')
             await client.send_message(message.channel, embed=em)
         elif message.content.startswith('>material'):
