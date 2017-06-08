3a793952814a976aff13d82b86b8454c7b81af29
Avery O <ave@ave.zone>
2017-06-08 08:06:37 +0300

    improve non-jpg and add avatar sbahjification

--- avebot.py
+++ avebot.py
@@ -221,20 +221,31 @@ async def sbahjify(contx):
     images_to_process = []
     for attach in contx.message.attachments:
         extension = os.path.splitext(attach['filename'])[1]
-        filename = "files/{}a{}".format(contx.message.id, extension)
+        filename = "files/{}att.{}".format(contx.message.id, extension).split('?')[0]
         download_file(attach['proxy_url'], filename)
-        if extension == ".png":
+        if extension != ".jpg" or extension != ".jpeg":
+            im = PIL.Image.open(filename)
+            new_name = filename.replace(extension, ".jpg")
+            im.save(new_name, "JPEG")
+            filename = new_name
+        images_to_process.append(filename)
+    for mention in contx.message.mentions:
+        linky = mention.avatar_url
+        extension = os.path.splitext(linky)[1].split('?')[0]
+        filename = "files/{}avi.{}".format(mention.id, extension)
+        download_file(linky, filename)
+        if extension != ".jpg" or extension != ".jpeg":
             im = PIL.Image.open(filename)
             new_name = filename.replace(extension, ".jpg")
             im.save(new_name, "JPEG")
             filename = new_name
         images_to_process.append(filename)
     stuff_after = contx.message.content.replace(prefix + "sbahjify", "").replace(" ", "")
-    if stuff_after != "":
-        extension = os.path.splitext(stuff_after)[1]
-        filename = "files/{}t{}".format(contx.message.id, extension)
+    if stuff_after != "" and stuff_after.startswith("http"):
+        extension = os.path.splitext(stuff_after)[1].split('?')[0]
+        filename = "files/{}txt.{}".format(contx.message.id, extension)
         download_file(stuff_after, filename)
-        if extension == ".png":
+        if extension != ".jpg" or extension != ".jpeg":
             im = PIL.Image.open(filename)
             new_name = filename.replace(extension, ".jpg")
             im.save(new_name, "JPEG")
@@ -246,6 +257,7 @@ async def sbahjify(contx):
                                  msg_to_send.format(contx.message.author.mention))
 
     for imgtp in images_to_process:
+        avelog("Processing {}".format(imgtp))
         im = PIL.Image.open(imgtp)
 
         for _ in range(2):
