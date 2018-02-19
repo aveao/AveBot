d811aed45912aef930705fdfeecda3cdb10aab50
Ave O <ave@ave.zone>
2018-02-19 11:44:54 +0300

    Improve jose number output

--- cogs/jose.py
+++ cogs/jose.py
@@ -9,12 +9,12 @@ class Jose:
     @commands.command(hidden=True)
     async def joseprob(self, ctx, jccount: float, amount: float):
         prob = (1 + (jccount / amount)) * 0.42
-        await ctx.send(f"Probability: `{prob:.1f}`")
+        await ctx.send(f"Probability: `{prob:.01f}`")
 
     @commands.command(hidden=True)
     async def josemax(self, ctx, jccount: float):
         maxfifty = (jccount / 4.58) * 0.42
-        await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty:.1f}`")
+        await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty:.01f}`")
 
 
 def setup(bot):
