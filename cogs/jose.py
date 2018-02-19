221538edcdf5e89c0ed8bbe45aca5e65e706f8e0
Ave O <ave@ave.zone>
2018-02-19 12:04:57 +0300

    jose: make jose commands public
    inb4 @lnmds kills me

--- cogs/jose.py
+++ cogs/jose.py
@@ -6,13 +6,15 @@ class Jose:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def joseprob(self, ctx, jccount: float, amount: float):
+        """Calculates probability of a jose steal."""
         prob = (1 + (jccount / amount)) * 0.42
         await ctx.send(f"Probability: `{prob:.01f}`")
 
-    @commands.command(hidden=True)
+    @commands.command()
     async def josemax(self, ctx, jccount: float):
+        """Gives the maximum JCs that'll give 50% chance"""
         maxfifty = (jccount / 4.58) * 0.42
         await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty:.01f}`")
 
