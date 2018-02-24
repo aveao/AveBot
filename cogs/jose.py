07bd8f33c818c10b311e9637e287a4e8106bc1b2
Ave O <ave@ave.zone>
2018-02-24 16:55:31 +0300

    jose: bugfix

--- cogs/jose.py
+++ cogs/jose.py
@@ -10,14 +10,14 @@ class Jose:
     @commands.command()
     async def joseprob(self, ctx, jccount: float, amount: float):
         """Calculates probability of a jose steal."""
-        prob = (1 + (Decimal(jccount) / Decimal(amount))) * 0.42
+        prob = (1 + (Decimal(jccount) / Decimal(amount))) * Decimal(0.42)
         prob = round(prob, 2)
         await ctx.send(f"Probability: `{prob}`")
 
     @commands.command()
     async def josemax(self, ctx, jccount: float):
         """Gives the maximum JCs that'll give 50% chance"""
-        maxfifty = (Decimal(jccount) / 4.58) * 0.42
+        maxfifty = (Decimal(jccount) / Decimal(4.58)) * Decimal(0.42)
         maxfifty = round(maxfifty, 2)
         await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty}`")
 
