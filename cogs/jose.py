52df171ca8ed0b0c4f0ef03ef060849bd1c4b3fd
Ave O <ave@ave.zone>
2018-02-24 16:52:45 +0300

    jose: fix rounding

--- cogs/jose.py
+++ cogs/jose.py
@@ -1,3 +1,4 @@
+from decimal import *
 import discord
 from discord.ext import commands
 
@@ -9,14 +10,16 @@ class Jose:
     @commands.command()
     async def joseprob(self, ctx, jccount: float, amount: float):
         """Calculates probability of a jose steal."""
-        prob = (1 + (jccount / amount)) * 0.42
-        await ctx.send(f"Probability: `{prob:.01f}`")
+        prob = (1 + (Decimal(jccount) / Decimal(amount))) * 0.42
+        prob = round(prob, 2)
+        await ctx.send(f"Probability: `{prob}`")
 
     @commands.command()
     async def josemax(self, ctx, jccount: float):
         """Gives the maximum JCs that'll give 50% chance"""
-        maxfifty = (jccount / 4.58) * 0.42
-        await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty:.01f}`")
+        maxfifty = (Decimal(jccount) / 4.58) * 0.42
+        maxfifty = round(maxfifty, 2)
+        await ctx.send(f"Maximum amount that gives 5 probability: `{maxfifty}`")
 
 
 def setup(bot):
